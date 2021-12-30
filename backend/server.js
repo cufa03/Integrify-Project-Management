529dc5ba1051e9fd775717f49afd8423a46265fa
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-12-30 21:44:13 +0100

    setup for backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const knex = require("knex");
+const app = express();
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: '5199',
+		database: 'project_management',
+	},
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.post("/app/v1/user/register", (req, res) => {
+  
+});
+app.get('/', (req, res) => { res.send("its working") });
+app.listen(5000, () => {
+	console.log('app is running on port 5000');
+});
